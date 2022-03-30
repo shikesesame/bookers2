452cb8f3daa3043bd8aadf4c8b92cb3d857ff6ae
EC2 Default User <ec2-user@ip-172-31-12-213.ec2.internal>
2022-03-30 02:57:52 +0000

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.new
+    @users = User.all
+    @books = @user.books
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  get "/homes/about" => "homes#about", as: "about"
+  resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
+  resources :users, only: [:index, :show, :edit, :create, :destroy, :update]
+end
